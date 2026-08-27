9fd20c6ffc93ee73c983553b006fb2411ad36628
bhanuka-viraj <bhanukaviraj22@gmail.com>
2026-08-27 18:45:16 +0530

    feat: add CrossOrigin annotations to controllers

--- src/main/java/com/ijse/media_service/controller/MediaController.java
+++ src/main/java/com/ijse/media_service/controller/MediaController.java
@@ -4,12 +4,14 @@ import com.ijse.media_service.dto.MediaUploadResponseDTO;
 import com.ijse.media_service.service.GcsStorageService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/media")
 public class MediaController {
